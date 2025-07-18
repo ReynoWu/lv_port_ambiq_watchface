612d26fd9bc67c1c4990d1596ac22b5148faf68f
Reyno Wu <rwu@ambiq.com>
2025-07-18 10:03:40 +0800

    watchface lvgl_watch_face works

--- lvgl_ambiq_demo/lvgl_watch_face/src/gui_task.c
+++ lvgl_ambiq_demo/lvgl_watch_face/src/gui_task.c
@@ -26,7 +26,29 @@
 #include "event_groups.h"
 #include "gui_task.h"
 #include "lvgl_test.h"
-#include "demos/lv_demos.h"
+#include "tsi_malloc.h"
+#include "nema_utils.h"
+// #include "lv_img_buf.h"
+#include "texture/TH_BG_454_bgra8888.h"
+#include "texture/TH_HOUR_bgra8888.h"
+#include "texture/TH_MINUTE_bgra8888.h"
+#include "texture/TH_SECOND_bgra8888.h"
+#include "texture/DOT_L_bgra8888.h"
+#include "texture/DOT_M_bgra8888.h"
+#include "texture/DOT_S_bgra8888.h"
+#include "texture/DOT_T_bgra8888.h"
+#include "texture/watch_bg.h"
+#include "texture/hour.h"
+#include "texture/minute.h"
+#include "texture/second.h"
+
+#ifndef LV_IMG_CF_TRUE_COLOR_ALPHA
+#   define LV_IMG_CF_TRUE_COLOR_ALPHA 5
+#endif
+
+#ifndef LV_IMG_PX_SIZE_ALPHA_BYTE
+#   define LV_IMG_PX_SIZE_ALPHA_BYTE 3
+#endif
 
 //*****************************************************************************
 //
@@ -114,6 +136,140 @@ void lv_example_loading_font(void)
     lv_obj_set_style_text_font(label3, my_font, 0);
     lv_obj_align(label3, LV_ALIGN_TOP_MID, 0, 150);
 }
+
+//*****************************************************************************
+//
+// Texture.
+//
+//*****************************************************************************
+lv_img_dsc_t img_watch_bg_psram = {
+//   .header.always_zero = 0,
+  .header.w = 454,
+  .header.h = 454,
+  .header.cf = LV_IMG_CF_TRUE_COLOR_ALPHA,
+};
+
+lv_img_dsc_t img_hour_psram = {
+//   .header.always_zero = 0,
+  .header.w = 50,
+  .header.h = 315,
+  .header.cf = LV_IMG_CF_TRUE_COLOR_ALPHA,
+};
+
+lv_img_dsc_t img_minute_psram = {
+//   .header.always_zero = 0,
+  .header.w = 50,
+  .header.h = 315,
+  .header.cf = LV_IMG_CF_TRUE_COLOR_ALPHA,
+};
+
+lv_img_dsc_t img_second_psram = {
+//   .header.always_zero = 0,
+  .header.w = 50,
+  .header.h = 315,
+  .header.cf = LV_IMG_CF_TRUE_COLOR_ALPHA,
+};
+
+lv_img_dsc_t img_dot_l_psram = {
+//   .header.always_zero = 0,
+  .header.w = 50,
+  .header.h = 50,
+  .data_size = 50 * 50 * LV_IMG_PX_SIZE_ALPHA_BYTE,
+  .header.cf = LV_IMG_CF_TRUE_COLOR_ALPHA,
+};
+
+lv_img_dsc_t img_dot_m_psram = {
+//   .header.always_zero = 0,
+  .header.w = 50,
+  .header.h = 50,
+  .data_size = 50 * 50 * LV_IMG_PX_SIZE_ALPHA_BYTE,
+  .header.cf = LV_IMG_CF_TRUE_COLOR_ALPHA,
+};
+
+lv_img_dsc_t img_dot_t_psram = {
+//   .header.always_zero = 0,
+  .header.w = 50,
+  .header.h = 50,
+  .data_size = 50 * 50 * LV_IMG_PX_SIZE_ALPHA_BYTE,
+  .header.cf = LV_IMG_CF_TRUE_COLOR_ALPHA,
+};
+
+lv_img_dsc_t img_dot_s_psram = {
+//   .header.always_zero = 0,
+  .header.w = 25,
+  .header.h = 28,
+  .data_size = 25 * 28 * LV_IMG_PX_SIZE_ALPHA_BYTE,
+  .header.cf = LV_IMG_CF_TRUE_COLOR_ALPHA,
+};
+
+//*****************************************************************************
+//
+// Load texture and font data from MRAM to PSRAM
+//
+//*****************************************************************************
+void
+texture_load(void)
+{
+    img_watch_bg_psram.data_size    = TH_BG_454_bgra8888_len;
+    img_hour_psram.data_size        = TH_HOUR_bgra8888_len;
+    img_minute_psram.data_size      = TH_MINUTE_bgra8888_len;
+    img_second_psram.data_size      = TH_SECOND_bgra8888_len;
+    img_dot_l_psram.data_size       = DOT_L_bgra8888_length;
+    img_dot_m_psram.data_size       = DOT_M_bgra8888_length;
+    img_dot_s_psram.data_size       = DOT_S_bgra8888_length;
+    img_dot_t_psram.data_size       = DOT_T_bgra8888_length;
+
+    img_watch_bg_psram.data = TH_BG_454_bgra8888; // (const uint8_t *)lv_malloc_core(img_watch_bg_psram.data_size);
+    if (img_watch_bg_psram.data == 0)
+    {
+        LV_LOG_INFO("img_watch_bg_psram.data error\n");
+    }
+    img_hour_psram.data     = TH_HOUR_bgra8888; // (const uint8_t *)lv_malloc_core(img_hour_psram.data_size);
+    if (img_hour_psram.data == 0)
+    {
+        LV_LOG_INFO("img_watch_bg_psram.data error\n");
+    }    
+    
+    img_minute_psram.data   = TH_MINUTE_bgra8888; // (const uint8_t *)lv_malloc_core(img_minute_psram.data_size);
+    if (img_minute_psram.data == 0)
+    {
+        LV_LOG_INFO("img_watch_bg_psram.data error\n");
+    }    
+    img_second_psram.data   = TH_SECOND_bgra8888; // (const uint8_t *)lv_malloc_core(img_second_psram.data_size);
+    if (img_second_psram.data == 0)
+    {
+        LV_LOG_INFO("img_watch_bg_psram.data error\n");
+    }
+    img_dot_l_psram.data    = DOT_L_bgra8888; // (const uint8_t *)lv_malloc_core(img_dot_l_psram.data_size);
+    if (img_dot_l_psram.data == 0)
+    {
+        LV_LOG_INFO("img_watch_bg_psram.data error\n");
+    }
+    img_dot_m_psram.data    = DOT_M_bgra8888; // (const uint8_t *)lv_malloc_core(img_dot_m_psram.data_size);
+    if (img_dot_m_psram.data == 0)
+    {
+        LV_LOG_INFO("img_watch_bg_psram.data error\n");
+    }
+    img_dot_s_psram.data    = DOT_S_bgra8888; // (const uint8_t *)lv_malloc_core(img_dot_s_psram.data_size);
+    if (img_dot_s_psram.data == 0)
+    {
+        LV_LOG_INFO("img_watch_bg_psram.data error\n");
+    }
+    img_dot_t_psram.data    = DOT_T_bgra8888; // (const uint8_t *)lv_malloc_core(img_dot_t_psram.data_size);
+    if (img_dot_t_psram.data == 0)
+    {
+        LV_LOG_INFO("img_dot_t_psram.data error\n");
+    }
+
+    // memcpy((void*)img_watch_bg_psram.data,  TH_BG_454_bgra8888, TH_BG_454_bgra8888_len);
+    // memcpy((void*)img_hour_psram.data,      TH_HOUR_bgra8888,   TH_HOUR_bgra8888_len);
+    // memcpy((void*)img_minute_psram.data,    TH_MINUTE_bgra8888, TH_MINUTE_bgra8888_len);
+    // memcpy((void*)img_second_psram.data,    TH_SECOND_bgra8888, TH_SECOND_bgra8888_len);
+    // memcpy((void*)img_dot_l_psram.data,    DOT_L_bgra8888, DOT_L_bgra8888_length);
+    // memcpy((void*)img_dot_m_psram.data,    DOT_M_bgra8888, DOT_M_bgra8888_length);
+    // memcpy((void*)img_dot_s_psram.data,    DOT_S_bgra8888, DOT_S_bgra8888_length);
+    // memcpy((void*)img_dot_t_psram.data,    DOT_T_bgra8888, DOT_T_bgra8888_length);
+}
 //*****************************************************************************
 //
 // Task function.
@@ -163,15 +319,102 @@ GuiTask(void *pvParameters)
     }
 
     // lv_demo_benchmark();
-    lv_demo_music();
-    //lv_demo_scroll();
+    // lv_demo_music();
+    // lv_demo_scroll();
     //lv_demo_vector_graphic_not_buffered();
     //lv_example_style_5();
     // lv_example_loading_font();
     // lv_example_gif_1();
     // lv_example_lodepng_1();
+
+    // Load texture.
+    // texture_load();
+
+        /*Now create the actual image*/
+    lv_obj_t *obj_watch_bg = lv_img_create(lv_scr_act());
+    lv_img_set_src(obj_watch_bg, &watch_bg);
+    lv_obj_align(obj_watch_bg, LV_ALIGN_TOP_LEFT, 0, 0);
+
+    lv_obj_t *lv_hour = lv_img_create(obj_watch_bg);
+    lv_img_set_src(lv_hour, &hour);
+    lv_obj_align(lv_hour, LV_ALIGN_TOP_LEFT, 202, 0);
+    lv_img_set_pivot(lv_hour, 25, 224);
+
+    lv_obj_t *lv_minute = lv_img_create(obj_watch_bg);
+    lv_img_set_src(lv_minute, &minute);
+    lv_obj_align(lv_minute, LV_ALIGN_TOP_LEFT, 202, 0);
+    lv_img_set_pivot(lv_minute, 25, 224);
+
+    lv_obj_t *lv_second = lv_img_create(obj_watch_bg);
+    lv_img_set_src(lv_second, &second);
+    lv_obj_align(lv_second, LV_ALIGN_TOP_LEFT, 202, 0);
+    lv_img_set_pivot(lv_second, 25, 224);
+
+    // lv_obj_t * lv_dot_l = lv_img_create(watch_bg);
+    // lv_img_set_src(lv_dot_l, &img_dot_l_psram);
+    // lv_obj_align(lv_dot_l, LV_ALIGN_CENTER, 0, 0);
+
+    // lv_obj_t * lv_dot_m = lv_img_create(watch_bg);
+    // lv_img_set_src(lv_dot_m, &img_dot_m_psram);
+    // lv_obj_align(lv_dot_m, LV_ALIGN_CENTER, 0, 0);
+
+    // lv_obj_t * lv_dot_s = lv_img_create(watch_bg);
+    // lv_img_set_src(lv_dot_s, &img_dot_s_psram);
+    // lv_obj_align(lv_dot_s, LV_ALIGN_CENTER, 0, 0);
+
+    // lv_obj_t * lv_dot_t = lv_img_create(watch_bg);
+    // lv_img_set_src(lv_dot_t, &img_dot_t_psram);
+    // lv_obj_align(lv_dot_t, LV_ALIGN_CENTER, 0, 0);
+
+    uint32_t last_sec = 0;
+    uint32_t current_sec = 0;
+    bool watch_update = false;
+    uint32_t needle_hour = 0;
+    uint32_t needle_minute = 0;
+    uint32_t needle_second = 0;
+    uint32_t angle;
+
     while(1)
     {
+        // current_sec = (uint32_t) nema_get_time();
+        current_sec += 1;
+
+        if (current_sec != last_sec)
+        {
+            last_sec = current_sec;
+            watch_update = true;
+
+            if (++needle_second > 59)
+            {
+                needle_second = 0;
+                needle_minute++;
+
+                if (needle_minute > 59)
+                {
+                    needle_minute = 0;
+                    needle_hour++;
+                    if (needle_hour > 11)
+                    {
+                        needle_hour = 0;
+                    }
+                }
+            }
+        }
+
+        if (watch_update == true)
+        {
+            angle = needle_hour * 300 + needle_minute * 5;
+            lv_img_set_angle(lv_hour, angle);
+
+            angle = needle_minute * 60 + needle_second;
+            lv_img_set_angle(lv_minute, angle);
+
+            angle = needle_second * 60;
+            lv_img_set_angle(lv_second, angle);
+
+            watch_update = false;
+        }
+
         uint32_t time_till_next;
 
         // Run timer handler.
